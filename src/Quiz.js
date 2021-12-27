bb348bd8bd18480c23dc6fb2f8dc1c6e7a83d0f5
Karppinen Pasi (EXT) <pasi.karppinen@kone.com>
2021-12-27 16:09:52 +0200

    Module 4 fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
 import QuizEnd from './QuizEnd.js';
 
-let quizData = require('./quiz_data.json');
+let quizData = require('./quiz_data.json');   
 
 class Quiz extends Component {
+    isQuizEnd;
+
     constructor(props) {
         super(props);
         this.state = {quiz_position: 1};
+        this.isQuizEnd = (this.state.quiz_position) - 1 == quizData.quiz_questions.length;
     }
 
+    
     render() {
-        if ((this.state.quiz_position) - 1 == quizData.quiz_questions.length) {
+        if (this.isQuizEnd) {
             return <div>                
                     <QuizEnd />                                
                 </div>

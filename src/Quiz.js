dd9ce619333ccd335dfbaa14259968843a96aa84
Karppinen Pasi (EXT) <pasi.karppinen@kone.com>
2021-12-27 16:11:26 +0200

    Module 4 fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,17 +5,15 @@ import QuizEnd from './QuizEnd.js';
 let quizData = require('./quiz_data.json');   
 
 class Quiz extends Component {
-    isQuizEnd;
-
     constructor(props) {
         super(props);
-        this.state = {quiz_position: 1};
-        this.isQuizEnd = (this.state.quiz_position) - 1 == quizData.quiz_questions.length;
+        this.state = {quiz_position: 1};      
     }
 
-    
     render() {
-        if (this.isQuizEnd) {
+        const isQuizEnd = (this.state.quiz_position) - 1 == quizData.quiz_questions.length;
+        
+        if (isQuizEnd) {
             return <div>                
                     <QuizEnd />                                
                 </div>
